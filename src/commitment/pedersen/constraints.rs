26ff7afcf30c201d013d60688c721cce6fefd7d4
Ihyun Nam <ihyunnam@DN51vsuc.SUNet>
2024-08-24 09:38:04 -0700

    .

--- src/commitment/pedersen/constraints.rs
+++ src/commitment/pedersen/constraints.rs
@@ -90,7 +90,7 @@ where
         let rand_bits: Vec<_> =
             r.0.iter()
                 .flat_map(|byte| {
-                    println!("byte.to_bits_le() {:?}", byte.to_bits_le().value());
+                    println!("byte.to_bits_le() {:?}", byte.to_bits_le());
                     byte.to_bits_le().unwrap()
                 })
                 .collect();
